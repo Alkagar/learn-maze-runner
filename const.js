c0340be7ebcb591f3fa05827a03316597fbfa103
Jakub Mrowiec <alkagar@gmail.com>
2015-09-23 16:24:36 +0200

    initial

---
+++ const.js
@@ -0,0 +1,13 @@
+var i = 0;
+var Directions = {
+    'UP': 'UP',
+    'DOWN': 'DOWN',
+    'LEFT': 'LEFT',
+    'RIGHT': 'RIGHT',
+};
+var PossibleDirections = Object.keys(Directions);
+
+module.exports = {
+    Directions: Directions,
+    PossibleDirections: PossibleDirections,
+};
